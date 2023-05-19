50f8a7e51237ccb7706d95d2d88747d405de133b
Andrey Freitas <yanusckiewicz@profusion.mobi>
2023-05-18 21:32:20 -0300

    test(DatePicker): add datepicker test

--- app/src/androidTest/java/com/aelnonym/loginespresso/LoginActivityTest.kt
+++ app/src/androidTest/java/com/aelnonym/loginespresso/LoginActivityTest.kt
@@ -2,9 +2,13 @@ package com.aelnonym.loginespresso
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.PickerActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.matcher.IntentMatchers
-import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
@@ -20,14 +24,27 @@ class LoginActivityTest {
     fun testLoginSuccess() {
         Intents.init()
 
-        Espresso.onView(ViewMatchers.withId(R.id.editTextUsername))
+        Espresso.onView(withId(R.id.editTextUsername))
             .perform(ViewActions.replaceText("username"))
 
-        Espresso.onView(ViewMatchers.withId(R.id.editTextPassword))
+        Espresso.onView(withId(R.id.editTextPassword))
             .perform(ViewActions.replaceText("password"))
 
-        Espresso.onView(ViewMatchers.withId(R.id.buttonLogin)).perform(ViewActions.click())
+        Espresso.onView(withId(R.id.buttonLogin)).perform(ViewActions.click())
 
         Intents.intended(IntentMatchers.hasComponent(CadastroActivity::class.java.name))
+    }
+
+    @Test
+    fun testDatePicker() {
+        val ano = 1994
+        val mes = 1
+        val dia = 30
+
+        Espresso.onView(withId(R.id.datePicker)).perform(PickerActions.setDate(ano, mes, dia))
+
+        Espresso.onView(withId(R.id.dateButton)).perform(click())
+
+        Espresso.onView(withId(R.id.dateButton)).check(matches(withText("$dia/$mes/$ano")))
     }
 }
--- app/src/main/java/com/aelnonym/loginespresso/LoginActivity.kt
+++ app/src/main/java/com/aelnonym/loginespresso/LoginActivity.kt
@@ -3,12 +3,15 @@ package com.aelnonym.loginespresso
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.DatePicker
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var usernameEditText: EditText
     private lateinit var passwordEditText: EditText
+    private lateinit var datePicker: DatePicker
+    private lateinit var dateButton: Button
     private lateinit var loginButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +27,16 @@ class LoginActivity : AppCompatActivity() {
 
             abrirTelaPrincipal()
         }
+        datePicker = findViewById(R.id.datePicker)
+        dateButton = findViewById(R.id.dateButton)
+
+        dateButton.setOnClickListener {
+            dateButton.text = datePicker.getString()
+        }
+    }
+
+    fun DatePicker.getString(): String {
+        return "$dayOfMonth/${month + 1}/$year"
     }
 
     private fun abrirTelaPrincipal() {
